627918b9d269f2e571a7e67a4afcbd322fd80eec
happy-jmalone <jmaloneweb@gmail.com>
2015-06-24 16:11:57 -0700

    tabs to spaces for better github readability

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,18 @@ var router   = express.Router();
 
 // CORS config
 var corsConfig = {
-	origin:         '*', // Any origin
-	methods:        ['GET', 'OPTIONS'],
-	allowedHeaders: ['Content-Type', 'X-Requested-With']
+    origin:         '*', // Any origin
+    methods:        ['GET', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'X-Requested-With']
 }
 
 // Basic index route
 router.get('/', cors(corsConfig), function (request, response) {
 
-	response.json({
-		title: 'NodeJS / Express / Mongo(ose) Demo',
-		author: 'Jake Malone <jmaloneweb@gmail.com>'
-	});
+    response.json({
+        title: 'NodeJS / Express / Mongo(ose) Demo',
+        author: 'Jake Malone <jmaloneweb@gmail.com>'
+    });
 
 });
 
